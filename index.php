3f01fb9de80a41c87705c7dde1603e38f1ebed89
Ramiro Manoel <ramiro.manoel77@gmail.com>
2023-01-26 00:22:41 -0300

    2o echo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
     echo "oi gente, nao estou conseguindo mexer no git da maneira correta";
 
-echo "mas pode ficar sereno q agr eu entendi melhor";
+echo "mas pode ficar sereno q agr eu entendi melhor, mas aqui não funcionou muito bem nao";
     
     
 ?>
