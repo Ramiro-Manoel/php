d5b4c563c68060e55d1df9645dd7e863d7776388
Ramiro-Manoel <98917766+Ramiro-Manoel@users.noreply.github.com>
2023-01-26 00:14:55 -0300

    mudan

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-    echo "oi gente, nao estou conseguindo mexer no git da maneira correnta";
+    echo "oi gente, nao estou conseguindo mexer no git da maneira correta";
+
+echo "mas pode ficar sereno q agr eu entendi melhor";
     
     
 ?>
