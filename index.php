74ce8e900effc72107903954c4c6038a24475b63
Ramiro-Manoel <98917766+Ramiro-Manoel@users.noreply.github.com>
2023-01-26 00:26:38 -0300

    7890 no primeiro echo

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-    echo "oi gente, nao estou conseguindo mexer no git da maneira correta";
+    echo "oi gente, nao estou conseguindo mexer no git da maneira correta7890";
 
 echo "mas pode ficar sereno q agr eu entendi melhor";
     
-    echo "acho que to pegando";
+ 
 
 ?>
