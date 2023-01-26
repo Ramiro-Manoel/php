6683abc4f49139c2701fed264c1577a6b9e50f8d
Ramiro Manoel <ramiro.manoel77@gmail.com>
2023-01-26 00:30:01 -0300

    echo1 com 12345

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    echo "oi gente, nao estou conseguindo mexer no git da maneira correta";
+    echo "oi gente, nao estou conseguindo mexer no git da maneira correta12345";
 
 echo "mas pode ficar sereno q agr eu entendi melhor, mas aqui não funcionou muito bem nao";
     
